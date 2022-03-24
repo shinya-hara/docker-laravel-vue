6e812e8a44a519aa120e15f90af21615badc2228
Shinya Hara <rabby.coffee.c5@gmail.com>
2022-03-25 08:38:06 +0900

    feat: Implement update student

--- src/app/Http/Controllers/ApiController.php
+++ src/app/Http/Controllers/ApiController.php
@@ -35,7 +35,20 @@ class ApiController extends Controller
     }
 
     public function updateStudent(Request $request, $id) {
+        if (Student::where('id', $id)->exists()) {
+            $student = Student::find($id);
+            $student->name = is_null($request->name) ? $student->name : $request->name;
+            $student->course = is_null($request->course) ? $student->course : $request->course;
+            $student->save();
 
+            return response()->json([
+                'message' => 'Student updated successfully'
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Student not found'
+            ], 404);
+        }
     }
 
     public function deleteStudent($id) {
