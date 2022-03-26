46919433572b820a2480646577e6a84b009ed5aa
Shinya Hara <rabby.coffee.c5@gmail.com>
2022-03-26 16:51:36 +0900

    chore: Refine UI

--- src/resources/views/tasks/index.blade.php
+++ src/resources/views/tasks/index.blade.php
@@ -25,14 +25,14 @@
         <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
                 <div class="flex gap-4">
                     <div class="w-64">
-                        <div class="border border-gray-100 rounded shadow">
-                            <div class="p-2 text-gray-500 bg-gray-200 rounded-t">
+                        <div class="border border-gray-100 shadow">
+                            <div class="p-2 text-gray-500 bg-gray-200 ">
                                 フォルダ
                             </div>
                             <div class="border-b border-inherit p-2 bg-white">
-                                <button class="px-2 py-1 border bg-gray-50 border-inherit text-gray-500 rounded hover:bg-gray-100 w-full">フォルダを追加</button>
+                                <button class="px-2 py-1 border bg-gray-50 border-inherit text-gray-500 hover:bg-gray-100 w-full">フォルダを追加</button>
                             </div>
-                            <div class="flex flex-col bg-white rounded-b divide-y divide-inherit">
+                            <div class="flex flex-col bg-white divide-y divide-inherit">
                                 @foreach($folders as $folder)
                                     <a href="{{ route('tasks.index', ['id' => $folder->id]) }}" class="p-2 {{ $current_folder_id === $folder->id ? 'bg-blue-400 text-white' : '' }}">
                                         {{ $folder->title }}
